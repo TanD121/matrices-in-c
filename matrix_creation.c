9cef0000734704c70e6a8ac679864a0d636887cc
TanD121 <tdeshpande2994@gmail.com>
2021-09-30 17:47:59 +0530

    added matrix input and display routines

--- matrix_creation.c
+++ matrix_creation.c
@@ -7,7 +7,52 @@ void main()
 	int result[3][3] = {0};
 	int i,j;
 
+	printf("\nEnter first matrix\n");
+
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("\nEnter the element at row %d & column %d : ",i+1,j+1);
+			scanf("%d",&m1[i][j]);
+		}
+	}
+
+	printf("\nThe first matrix is\n");
+
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("\t%d ",m1[i][j]);
+		}
+
+		printf("\n");
+	}
 
 
+	printf("\nEnter Second matrix\n");
+
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("\nEnter the element at row %d & column %d : ",i+1,j+1);
+			scanf("%d",&m2[i][j]);
+		}
+	}
+
+
+	printf("\nThe Second matrix is\n");
+
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("\t%d ",m2[i][j]);
+		}
+
+		printf("\n");
+	}
 
 }
