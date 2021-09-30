bb93f27c42892071b0897c6667b5a45a9a3268a3
TanD121 <tdeshpande2994@gmail.com>
2021-09-30 23:40:11 +0530

    Matrix multiplication routines added

--- matrix_creation.c
+++ matrix_creation.c
@@ -4,8 +4,10 @@ void main()
 {
 	int m1[3][3] = {0};
 	int m2[3][3] = {0};
-	int result[3][3] = {0};
-	int i,j;
+	int result[3][3] = {0}; // matrix to store addition result
+	int mul_result[3][3] = {0}; //matrix to store multiplication result
+
+	int i,j,k = 0;                  // Iterator variables
 
 	printf("\nEnter first matrix\n");
 
@@ -55,7 +57,7 @@ void main()
 		printf("\n");
 	}
 
-	// Addition of 2 matrices 
+	// Addition of 2 matrices --------------------------------------------------------------------------------------------------------
 	
 	printf("\nAddition of the 2 input matrices is:\n");
 
@@ -69,4 +71,20 @@ void main()
 		printf("\n");
 	}
 
+	// Multiplication of 2 matrices-----------------------------------------------------------------------------------------------------
+	
+	printf("\nMultiplication of the 2 input matrices is:\n");
+
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			for(k=0;k<3;k++)
+			{
+				mul_result[i][j] = mul_result[i][j] + m1[i][k]*m2[k][j];
+			}
+			printf("\t%d",mul_result[i][j]);
+		}
+		printf("\n");
+	}
 }
